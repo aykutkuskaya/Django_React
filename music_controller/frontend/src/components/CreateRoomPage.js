44ccee9307e92a216a2a26e5f13820438ba23660
Aykut Kuşkaya <aykutkuskaya58@hotmail.com>
2021-09-26 22:17:24 +0300

    CreateRoom,RoomJoin pagesa dded

---
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+
+export default class CreateJoinPage extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+        <p>
+        This is Room Create Page
+        </p>);
+    }
+}
